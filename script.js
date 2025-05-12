0615ccb0ccb9a8caa938616ec6797fe548662273
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2025-05-11 19:49:18 -0700

    work

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-window.addEventListener("mousedown", (e) => {
-    document.querySelector("nav").classList.toggle("expand"); // to expand
 
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
+  if(scrollTop <= 5){
+    document.querySelector("nav").classList.remove("expand");
+  }
+  else if(scrollTop >= 104){
+    document.querySelector("nav").classList.add("expand");
+  }
 });
