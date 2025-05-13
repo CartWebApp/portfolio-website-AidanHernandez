c17711ac8d3f87e403f474ef30a842e56c8b784b
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2025-05-12 21:13:39 -0700

    lots of work

--- js/pageFour.js
+++ js/pageFour.js
@@ -1,27 +1,5 @@
 let carousel = document.getElementById("carousel")
 
-window.addEventListener("click", () => {
-    if(carousel.classList.contains("cycleZero")){
-        carousel.classList.toggle("cycleZero")
-        carousel.classList.toggle("cycleOne")
-    }
-    else if(carousel.classList.contains("cycleOne")){
-        carousel.classList.toggle("cycleOne")
-        carousel.classList.toggle("cycleTwo")
-    }
-    else if(carousel.classList.contains("cycleTwo")){
-        carousel.classList.toggle("cycleTwo")
-        carousel.classList.toggle("cycleThree")
-    }
-    else if(carousel.classList.contains("cycleThree")){
-        carousel.classList.toggle("cycleThree")
-        carousel.classList.toggle("cycleFour")
-    }
-    else if(carousel.classList.contains("cycleFour")){
-        carousel.classList.toggle("cycleFour")
-        carousel.classList.toggle("cycleZero")
-    }
-})
 
 
 function turnMechanic(){
